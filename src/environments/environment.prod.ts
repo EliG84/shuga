4a68d2244c33cafce9eec8176e45958c18d6cc20
Eli <elionlink@gmail.com>
2021-10-16 18:37:31 +0300

    Client version: 001.000.003

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     // BASE_URL: 'https://sugartrackapi.sababa.digital/api', // namecheap
     // BASE_URL: 'https://sugartrackapi-qa.sababa.digital/api', // namecheap-qa
     BASE_URL: 'https://sugartrackapi-prod.sababa.digital/api', // namecheap-prod
-    APP_VERSION: '001.000.002',
+    APP_VERSION: '001.000.003',
   production: true
 };
